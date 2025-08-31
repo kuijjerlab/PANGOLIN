72b34e7655a019aa04d13f07a9fc818da952b04c
aiwa1986 <tatiana.belova@nmbu.no>
2025-08-31 15:58:59 +0200

    changed the script to take the gdc files as input

--- workflow/bin/combine_allTCGA_expression.R
+++ workflow/bin/combine_allTCGA_expression.R
@@ -1,88 +1,87 @@
-
 #####################
 ## Load R packages ##
 #####################
-# List of packages to be loaded
 required_libraries <- c("TCGAbiolinks", "SummarizedExperiment", 
-            "dplyr", "DT", "optparse", "data.table")
+                        "dplyr", "DT", "optparse", "data.table")
 
 for (lib in required_libraries) {
-  suppressPackageStartupMessages(library(lib, character.only = TRUE,
-                                quietly = TRUE))
+    suppressPackageStartupMessages(library(lib, character.only = TRUE,
+                                  quietly = TRUE))
 }
+
 ####################
 ## Read arguments ##
 ####################
-### Options
 options(stringsAsFactors = FALSE)
-### Command line options
+
 option_list <- list(
     optparse::make_option(
-        c("-d", "--expression_dir"),
+        c("-f", "--gdc_files"),
         type = "character",
         default = NULL,
-        help = "Path to the directory with expression files.",
-        metavar = "character"),
+        help = "Comma or space-separated list of GDC RData files.",
+        metavar = "character"
+    ),
     optparse::make_option(
         c("-e", "--combined_expression_file"),
-            type = "character",
-            default = NULL,
-            help = "Path to the output combined expression file.",
-            metavar = "character"),
+        type = "character",
+        default = NULL,
+        help = "Path to the output combined expression file.",
+        metavar = "character"
+    ),
     optparse::make_option(
         c("-g", "--group_file"),
         type = "character",
         default = NULL,
         help = "Path to the output group file.",
-        metavar = "character"),
+        metavar = "character"
+    ),
     optparse::make_option(
-        c("-f", "--feature_file"),
+        c("-r", "--feature_file"),
         type = "character",
         default = NULL,
         help = "Path to the output feature file.",
-        metavar = "character")
+        metavar = "character"
+    )
 )
 
-### Parse options ###
 opt_parser <- optparse::OptionParser(option_list = option_list)
 opt <- optparse::parse_args(opt_parser)
 
-### Initialize variable ###
-
-EXPRESSION_DIR <- opt$expression_dir
+# Parse file list
+GDC_FILES <- opt$gdc_files
 OUTPUT_EXPRESSION_FILE <- opt$combined_expression_file
 GROUP_FILE <- opt$group_file
 FEATURE_FILE <- opt$feature_file
 
+# Convert space-separated string to vector
+exp_files <- unlist(strsplit(GDC_FILES, " "))
+exp_files <- exp_files[exp_files != ""]  # Remove empty strings
 
 ### load functions ###
 source("workflow/bin/download_gdc_fn.R")
-exp_files <- list.files(EXPRESSION_DIR, pattern = ".RData")
-exp_files
 
 ### combine summarized experiments in one matrix ###
-exp <- NULL
 exp_all <- NULL
-groups <- NULL
 groups_all <- NULL
 
 for (i in 1:length(exp_files)) {
-        project_id <- gsub(".RData", "", exp_files[i])
-        exp <- readSummarizedExperiment(file.path(EXPRESSION_DIR, exp_files[i]))
-        exp_all <- cbind(exp, exp_all)
-        samples <- colnames(exp)
-        groups <- data.table("sample_id" = samples, "project" = project_id)
-        groups_all <- rbind(groups, groups_all)
+    project_id <- gsub(".RData", "", basename(exp_files[i]))
+    exp <- readSummarizedExperiment(exp_files[i])  # Use full path
+    exp_all <- cbind(exp, exp_all)
+    samples <- colnames(exp)
+    groups <- data.table("sample_id" = samples, "project" = project_id)
+    groups_all <- rbind(groups, groups_all)
 }
+
 write.table(exp_all,
             file = OUTPUT_EXPRESSION_FILE,
-            col.names = T, row.names = T, sep = "\t", quote = F)
-
+            col.names = TRUE, row.names = TRUE, sep = "\t", quote = FALSE)
 
 write.table(groups_all, GROUP_FILE,
-            col.names = F, row.names = F, sep = "\t", quote = FALSE)
+            col.names = FALSE, row.names = FALSE, sep = "\t", quote = FALSE)
 
-load(file.path(EXPRESSION_DIR, exp_files[1]), data <- new.env())
+load(exp_files[1], data <- new.env())
 data <- data[["mrna_df"]]
 features <- rowRanges(data)
 save(features, file = FEATURE_FILE)
