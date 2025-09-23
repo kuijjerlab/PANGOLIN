d3ffe59104d363404b6b6ef2da21be4fcc1d8bf2
aiwa1986 <tatiana.belova@nmbu.no>
2025-09-23 09:28:05 +0200

    input changed from a directory to files

--- workflow/bin/select_best_k_cola.R
+++ workflow/bin/select_best_k_cola.R
@@ -1,74 +1,93 @@
 #####################
 ## Load R packages ##
 #####################
-
 required_libraries <- c("data.table", "optparse", "tidyr", "dplyr",
-                "cola", "stringr", "tidyverse", "viridis", "ggplot2")
+                        "cola", "stringr", "tidyverse", "viridis", "ggplot2")
 for (lib in required_libraries) {
-  suppressPackageStartupMessages(library(lib, character.only = TRUE,
-                                quietly = TRUE))
+    suppressPackageStartupMessages(
+        library(lib, character.only = TRUE, quietly = TRUE)
+    )
 }
 ####################
 ## Read arguments ##
 ####################
 option_list = list(
     make_option(
-        c("-d", "--tumor_dir"),
+        c("-i", "--indegree_best_k_files"),
         type = "character",
         default = NULL,
-        help = "Path to the the main tumor directory.",
-        metavar = "character"),
-   make_option(
-        c("-i", "--best_cola_k_indegree"),
-        type = "character",
-        default = NULL,
-        help = "Path to a file containing the best information 
-                about selected number of clusters for indegree.",
+        help = "Comma-separated list of files 
+          containing results of indegree cola clustering.",
         metavar = "character"),
     make_option(
-        c("-e", "--best_cola_k_expression"),
+        c("-e", "--expression_best_k_files"),
         type = "character",
         default = NULL,
-        help = "Path to a file containing the best information 
-                about selected number of clusters for expression.",
-        metavar = "character"))
-
+        help = "Comma-separated list of files 
+          containing results of expression cola clustering.",
+        metavar = "character"),
+    make_option(
+        c("--best_cola_k_indegree"),
+        type = "character",
+        default = NULL,
+        help = "Path to a file to save the 
+          combined best k information for indegree.",
+        metavar = "character"),
+    make_option(
+        c("--best_cola_k_expression"),
+        type = "character",
+        default = NULL,
+        help = "Path to a file to save the 
+          combined best k information for expression.",
+        metavar = "character")
+)
 
 opt_parser = OptionParser(option_list = option_list)
 opt = parse_args(opt_parser)
 
-## Initialize variable
-TUMOR_DIR_MAIN <- opt$tumor_dir
+## Initialize variables
+INDEGREE_K_FILES <- unlist(strsplit(opt$indegree_best_k_files, " "))
+EXPRESSION_K_FILES <- unlist(strsplit(opt$expression_best_k_files, " "))
+
 BEST_K_IND <- opt$best_cola_k_indegree
 BEST_K_EXP <- opt$best_cola_k_expression
 
 source("workflow/bin/cola_clustering_fn.R")
-##### Load in INDEGREE cluster results
-result_files_indegree <-
-        list.files(TUMOR_DIR_MAIN, 
-        recursive = TRUE,
-        full = TRUE, pattern = "best_k_indegree")
-res_indegree_list <- lapply(result_files_indegree, function(file) {
+
+##############################
+## Process INDEGREE Results ##
+##############################
+if (length(INDEGREE_K_FILES) > 0) {
+  cat("Processing indegree best k files...\n")
+  res_indegree_list <- lapply(INDEGREE_K_FILES, function(file) {
     load_result(file, object_name = "res_k")
-})
-res_all_indegree <- do.call(rbind, res_indegree_list)
-combined_res_indegree <- combine_k_results(res_all_indegree)
-write.table(combined_res_indegree, 
-            BEST_K_IND,
-            col.names = T, row.names = F, sep = "\t", quote = F)
+  })
+  res_all_indegree <- do.call(rbind, res_indegree_list)
+  combined_res_indegree <- combine_k_results(res_all_indegree)
+  write.table(combined_res_indegree, 
+              BEST_K_IND,
+              col.names = TRUE, row.names = FALSE, sep = "\t", quote = FALSE)
+  cat("Indegree results saved to:", BEST_K_IND, "\n")
+} else {
+  cat("No indegree files provided.\n")
+}
 
-
-
-#### Load in EXPRESSION cluster results
-result_files_expression <-
-        list.files(TUMOR_DIR_MAIN, 
-        recursive = TRUE,
-        full = TRUE, pattern = "best_k_expression")
-res_expression_list <- lapply(result_files_expression, function(file) {
+################################
+## Process EXPRESSION Results ##
+################################
+if (length(EXPRESSION_K_FILES) > 0) {
+  cat("Processing expression best k files...\n")
+  res_expression_list <- lapply(EXPRESSION_K_FILES, function(file) {
     load_result(file, object_name = "res_k")
-})
-res_all_expression <- do.call(rbind, res_expression_list)
-combined_res_expression <- combine_k_results(res_all_expression)
-write.table(combined_res_expression, 
-            BEST_K_EXP,
-            col.names = T, row.names = F, sep = "\t", quote = F)
+  })
+  res_all_expression <- do.call(rbind, res_expression_list)
+  combined_res_expression <- combine_k_results(res_all_expression)
+  write.table(combined_res_expression,
+              BEST_K_EXP,
+              col.names = TRUE, row.names = FALSE, sep = "\t", quote = FALSE)
+  cat("Expression results saved to:", BEST_K_EXP, "\n")
+} else {
+  cat("No expression files provided.\n")
+}
+
+
