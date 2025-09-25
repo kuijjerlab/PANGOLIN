5926fa3865c79a610fa9c08c674d579717996d36
aiwa1986 <tatiana.belova@nmbu.no>
2025-09-25 11:56:44 +0200

    input changed from a directory to files

--- workflow/bin/cox_regression_tumor.R
+++ workflow/bin/cox_regression_tumor.R
@@ -24,10 +24,16 @@ option_list <- list(
         help = "Path to the clinical file.",
         metavar = "character"),
     optparse::make_option(
-        c("-p", "--tumor_pd1_dir"),
+        c("-l", "--pd1_links_file"),
         type = "character",
         default = NULL,
-        help = "Path to the pd1 tumor directory.",
+        help = "Path to the PD1 links file.",
+        metavar = "character"),
+    optparse::make_option(
+        c("-n", "--pd1_net_file"),
+        type = "character",
+        default = NULL,
+        help = "Path to the PD1 network file.",
         metavar = "character"),
     optparse::make_option(
         c("-f", "--number_folds"),
@@ -54,10 +60,10 @@ option_list <- list(
         help = "Alpha value for the elastic net.",
         metavar = "numeric"),
     optparse::make_option(
-        c("-o", "--output_file"),
+        c("-o", "--output"),
         type = "character",
         default = NULL,
-        help = "Path to the output_file",
+        help = "Path to the output file",
         metavar = "character"))
 
 opt_parser <- optparse::OptionParser(option_list = option_list)
@@ -65,21 +71,28 @@ opt <- optparse::parse_args(opt_parser)
 
 ## Initialize variable
 TUMOR_CLIN_FILE <- opt$tumor_clin_file_path
-TUMOR_PD1_DIR <- opt$tumor_pd1_dir
+PD1_LINKS_FILE <- opt$pd1_links_file
+PD1_NET_FILE <- opt$pd1_net_file
 NUMBER_FOLDS <- opt$number_folds
 NUMBER_TIMES <- opt$number_times
 NUMBER_CORES <- opt$number_cores
 ALPHA <- opt$alpha
-OUTPUT_FILE <- opt$output_file
+OUTPUT_FILE <- opt$output
 
+####################
 source("workflow/bin/cox_regression_tumor_fn.R")
 dir.create(dirname(OUTPUT_FILE), recursive = TRUE, showWarnings = FALSE)
 res <- run_glmnet_ntimes_pd1(tumor_clin_file_path = TUMOR_CLIN_FILE,
-                    tumor_pd1_dir = TUMOR_PD1_DIR,
+                    pd1_links_file = PD1_LINKS_FILE,
+                    pd1_net_file = PD1_NET_FILE,
+                    pd1_scores_file = NULL,
+                    pdl1_expression_file = NULL,
+                    cluster_file = NULL,
                     number_folds = NUMBER_FOLDS,
                     ntimes = NUMBER_TIMES,
                     ncores = NUMBER_CORES, 
                     alpha = ALPHA)
 write.table(res, OUTPUT_FILE, col.names = TRUE,
     sep = "\t", row.names = FALSE, quote = FALSE)
+
 
