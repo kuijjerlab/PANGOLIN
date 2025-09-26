54f9ce44eb49b4208f1320f8fbcedbf01dd56d9b
aiwa1986 <tatiana.belova@nmbu.no>
2025-09-26 13:32:54 +0200

    inputs are changed

--- workflow/bin/plot_PC_immune_correlations.R
+++ workflow/bin/plot_PC_immune_correlations.R
@@ -2,8 +2,8 @@
 ## Load R packages ##
 #####################
 # List of packages to be loaded
-required_libraries <- c("circlize", "data.table", 
-            "dplyr", "plyr", "ComplexHeatmap", "viridis")
+required_libraries <- c("optparse", "circlize", "data.table", 
+            "plyr", "dplyr", "ComplexHeatmap", "viridis")
 
 for (lib in required_libraries) {
   suppressPackageStartupMessages(library(lib, character.only = TRUE,
@@ -24,10 +24,10 @@ option_list <- list(
         help = "Path to the cox summary file.",
         metavar = "character"),
     optparse::make_option(
-        c("-d", "--tumor_dir"),
+        c("-d", "--combined_patient_data_files"),
         type = "character",
         default = NULL,
-        help = "Path to the the main tumor directory.",
+        help = "Path to the combined patient data files.",
         metavar = "character"),
     optparse::make_option(
         c("-o", "--output_file"),
@@ -41,14 +41,14 @@ opt <- optparse::parse_args(opt_parser)
 
 ## Initialize variable
 COX_SUMMARY_ALL <- opt$cox_summary_all_cancers
-TUMOR_DIR_MAIN <- opt$tumor_dir
+COMBINED_PATIENT_DATA_FILES <- opt$combined_patient_data_files
 OUTPUT_PNG_FILE <- opt$output_file
 
 source("workflow/bin/merge_patient_data_fn.R")
 source("workflow/bin/plotting_fn.R")
-
+COMBINED_PATIENT_DATA_FILES <- unlist(strsplit(COMBINED_PATIENT_DATA_FILES, " "))
 res <- generate_PC_immune_correlation_table(cox_results_file = COX_SUMMARY_ALL,
-                                cancer_dir = TUMOR_DIR_MAIN, 
+                                combined_patient_data_files = COMBINED_PATIENT_DATA_FILES, 
                                 pval_threshold = 0.05)
 png(OUTPUT_PNG_FILE, width = 2000, height = 2000, res = 200) 
 plot_pc_immune_correlations(res)
