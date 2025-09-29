25310220c4d75e01a30a851be0c0a8170fc74a55
ec-tatianub <ec-tatianub@gpu-3.fox>
2025-09-29 15:48:12 +0200

    documentation is fixed according to changes

--- workflow/bin/utils_pcp_fn.R
+++ workflow/bin/utils_pcp_fn.R
@@ -93,30 +93,41 @@ read_networks_norm <- function(network_files) {
 
 #' Run the PORCUPINE method on quantile normalized networks
 #'
-#' This function runs the PORCUPINE methods on networks and pathway data for
-#' a specific cancer type. It reads the network and edges files, filters the
+#' This function runs the PORCUPINE methods on networks and pathway data for a
+#' specific cancer type. It reads the network and edges files, filters the
 #' pathways, performs PCA on the pathways, and runs permutations to assess
 #' significance.
 #'
-#' @param cancer the type of cancer for which the analysis is to be performed.
-#' @param network_dir the directory containing the network files.
-#' @param edge_file A character string specifying the path to the file
-#'                  containing edges information.
-#' @param pathway_file A character string specifying the path to the file
-#'                     containing pathway information in GMT format.
-#' @param res_dir the directory where the results will be saved.
-#' @param ncores_to_use the number of cores to use for parallel processing.
-#' @return results are saved in a specified directory
+#' @param cancer Cancer type for which the analysis is performed.
+#' @param network_files Vector of paths to normalized network .RData files.
+#' @param edge_file Path to file containing edges information.
+#' @param pathway_file Path to file containing pathway information in GMT 
+#'                     format.
+#' @param ncores_to_use Number of cores to use for parallel processing.
+#' @param pathways_results_file Path to save pathway PCA results.
+#' @param pathways_results_random_file Path to save random permutation results.
+#' @param porcupine_results_file Path to save final PORCUPINE results.
+#' @param individual_scores_file Path to save individual pathway scores.
+#' @param minSize Minimum pathway size (default: 5).
+#' @param maxSize Maximum pathway size (default: 150).
+#' @param n_permutations Number of permutations (default: 1000).
+#' @return Results are saved to specified output files.
 #' @examples
+#' # Example usage (not run)
+#' @export
 
 runPORCUPINE_norm <- function(network_files,
                         cancer,
                         edge_file,
                         pathway_file,
-                        res_dir,
                         ncores_to_use,
+                        pathways_results_file,
+                        pathways_results_random_file,
+                        porcupine_results_file,
+                        individual_scores_file,
                         minSize = 5,
-                        maxSize = 150) {
+                        maxSize = 150,
+                        n_permutations = 1000) {
     message("Reading in network files", "\n")
     net <- read_networks_norm(network_files)
     message("Reading in edges file", "\n")
@@ -129,30 +140,37 @@ runPORCUPINE_norm <- function(network_files,
     pca_res_pathways <- pca_pathway(pathways_to_use, 
             net, edges, ncores_to_use, scale_data = TRUE)
     write.table(pca_res_pathways,
-                file.path(res_dir, paste0("pathways_results_", cancer,
-                                          ".txt")),
+                pathways_results_file,
                 col.names = T, row.names = F, sep = "\t", quote = FALSE)
+
+    message("Calculating individual scores", "\n")
+    ind_scores <- get_pathway_ind_scores(pathways,
+                        net,
+                        edges)
+    # Save individual scores
+    save(ind_scores, file = individual_scores_file)
+
     message("Running PORCUPINE permutations", "\n")
     pca_res_random <- pca_random(net, edges, pca_res_pathways, pathways,
-                                 n_perm = 1000, ncores = ncores_to_use,
+                                 n_perm = n_permutations, ncores = ncores_to_use,
                                  scale_data = TRUE)
 
     write.table(pca_res_random,
-                file.path(res_dir, paste0("pathways_results_random_",
-                                          cancer, ".txt")),
+                pathways_results_random_file,
                 col.names = T, row.names = F, sep = "\t", quote = FALSE)
+    message("Calculating PORCUPINE results", "\n")
     res_porcupine <- porcupine(pca_res_pathways, pca_res_random)
     res_porcupine$p.adjust <- p.adjust(res_porcupine$pval, method = "fdr")
     write.table(res_porcupine,
-                file.path(res_dir, paste0("pcp_results_", cancer, ".txt")),
+                porcupine_results_file,
                 col.names = T, row.names = F, sep = "\t", quote = FALSE)
 }
 
 #' Read Sample File
 #'
-#' This function reads a sample file
+#' This function reads a sample file.
 #'
-#' @param sample_file the path to the sample file to be read.
+#' @param sample_file Path to the sample file to be read.
 #' @return A data.table object containing the samples read from the file.
 #' @export
 #' 
@@ -164,27 +182,26 @@ read_sample_file <- function(sample_file) {
 
 #' Extract PD-1 Related Edges and Network Data
 #'
-#' This function extracts edges and network data related to PD-1 pathway
+#' This function extracts edges and network data related to PD-1 pathway.
 #'
-#' @param cancer the type of cancer for which the analysis is to be performed.
-#' @param network_dir the directory containing the network files.
-#' @param edge_file the path to the file containing edges information. The 
-#'                  file should have columns "reg" and "tar".
-#' @param pathway_file the path to the file containing pathway information in
-#'                     GMT format.
-#' @param sample_file the path to a file containing sample information.
-#' @param res_dir the directory where the extracted data will be saved.
-#' @return saves the extracted PD-1 related network and edges data to 
-#'         specified files in the result directory.
+#' @param cancer Cancer type for which the analysis is performed.
+#' @param network_files Vector of paths to normalized network .RData files.
+#' @param edge_file Path to file containing edges information. The file should
+#'                  have columns "reg" and "tar".
+#' @param pathway_file Path to file containing pathway information in GMT 
+#'                     format.
+#' @param pd1_edges_file Path to save extracted PD-1 edges data.
+#' @param pd1_net_file Path to save extracted PD-1 network data.
+#' @return Saves the extracted PD-1 related network and edges data to 
+#'         specified files.
 #' @export
 
 extract_pd1_edges_norm <- function(network_files,
                         cancer,
                         edge_file,
                         pathway_file,
-                        sample_file,
-                        res_dir) {
-    samples <- read_sample_file(sample_file)
+                        pd1_edges_file,
+                        pd1_net_file) {
     message("Reading in network files", "\n")
     net <- read_networks_norm(network_files)
     message("Reading in edges file", "\n")
@@ -195,25 +212,19 @@ extract_pd1_edges_norm <- function(network_files,
     idx <- which(edges$tar %in% pd1_genes)
     pd1_net <- net[idx,]
     links <- edges[idx,]
-    save(pd1_net, file = file.path(res_dir, paste0("pd1_net_norm_", cancer,
-                                                   ".RData")))
-    save(links, file = file.path(res_dir, paste0("pd1_edges_norm_", cancer,
-                                                 ".RData")))
+    save(pd1_net, file = pd1_net_file)
+    save(links, file = pd1_edges_file)
     }
-
-
 
 #' Calculate Indegree for Each Cancer
 #'
 #' This function calculates the indegree for each cancer by reading network 
 #' files and edge data.
 #'
-#' @param cancer the type of cancer for which the analysis is to be performed.
-#' @param network_dir The directory where the network files are located.
-#' @param edge_file The path to the txt file containing edges, with columns
-#'                  "reg" and "tar".
-#' @return Data frame. A table with the calculated indegrees for each
-#'         target.
+#' @param network_files Vector of paths to normalized network .RData files.
+#' @param edge_file Path to txt file containing edges, with columns "reg" and
+#'                  "tar".
+#' @return Data frame with calculated indegrees for each target.
 #' @export
 #'
 #' @import data.table
@@ -236,78 +247,4 @@ calculate_indegree_norm <- function(network_files,
 
 
 
-#' Extract mapping of individuals on PC1 for a pathway
-#' @param cancer cancer 
-#' @param network_dir Directory where the networks files are
-#' @param edge_file Txt file with edges, columns "reg", "tar"
-#' @param pathway_file Gmt file with pathways
-#' @param sample_file File with samples
-#' @param res_dir directory to store pcp results
-#' @return Saving the ouput of pcp
-#' @export
 
-extract_individul_mapping_norm <- function(network_files,
-                        cancer,
-                        edge_file,
-                        pathway_file,
-                        sample_dir,
-                        res_dir) {
-    sample_files <- list.files(sample_dir)
-    sample_file <- sample_files[grep(cancer, sample_files,
-                                     ignore.case = TRUE)]
-    load(file.path(sample_dir, sample_file), samples <- new.env())
-    samples <- samples[["samples"]]
-    message("Reading in network files", "\n")
-    net <- read_networks_norm(network_files)
-    colnames(net) <- samples$id
-    message("Reading in edges file", "\n")
-    edges <- fread(edge_file)
-    pathways <- load_gmt(pathway_file)
-    pathways <- filter_pathways(pathways, edges)
-    pathways_to_use <- filter_pathways_size(pathways, minSize = 5,
-                                             maxSize = 150)
-    ind_scores <- get_pathway_ind_scores(pathways,
-                        net,
-                        edges)
-    save(ind_scores, file = file.path(res_dir, paste0("individual_scores_",
-                                                       cancer, ".RData")))
-}
-
-
-
-#' Extract mapping of individuals on PC1 for a pathway
-#' @param cancer cancer 
-#' @param network_dir Directory where the networks files are
-#' @param edge_file Txt file with edges, columns "reg", "tar"
-#' @param pathway_file Gmt file with pathways
-#' @param sample_file File with samples
-#' @param res_dir directory to store pcp results
-#' @return Saving the ouput of pcp
-#' @export
-
-extract_feature_scores_norm <- function(network_files,
-                        cancer,
-                        edge_file,
-                        pathway_file,
-                        sample_dir,
-                        res_dir) {
-    sample_files <- list.files(sample_dir)
-    sample_file <- sample_files[grep(cancer, sample_files,
-                                     ignore.case = TRUE)]
-    load(file.path(sample_dir, sample_file), samples <- new.env())
-    samples <- samples[["samples"]]
-    message("Reading in network files", "\n")
-    net <- read_networks_norm(network_files)
-    colnames(net) <- samples$id
-    message("Reading in edges file", "\n")
-    edges <- fread(edge_file)
-    pathways <- load_gmt(pathway_file)
-    pathways <- filter_pathways(pathways, edges)
-    pathways_to_use <- filter_pathways_size(pathways, minSize = 5,
-                                             maxSize = 150)
-    feature_scores <- get_pathway_features_scores(pathways,
-                        net,
-                        edges)
-    save(feature_scores, file = file.path(res_dir, paste0("feature_scores_",
-                                                           cancer, ".RData")))
-}
